e9fb60d39cd0ec41ef372fb985bec867989b75b4
fbergot <florian.bergot564@gmail.com>
2022-02-17 00:50:25 +0100

    add Observer Pattern + small modif in Proxy

---
+++ behavioral_Design_Pattern/Observer/implementation1.js
@@ -0,0 +1,52 @@
+// ------------------------ Observer
+const Subject = class {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(callback => callback !== observer);
+    }
+    trigger(productName) {
+        this.observers.forEach(observer => {
+            observer.addToWishList(productName);
+        })
+    }
+}
+
+const WishList = class {
+    constructor() {
+        this.products = [];
+    }
+    addToWishList(productName) {
+        console.log(`add ${productName} in products`);
+        this.products.unshift(productName);
+    }
+}
+
+const Mailer = class {
+    constructor(email) {
+        this.email = email;
+    }
+    addToWishList(productName) {
+        console.log(`Send email to ${this.email} with ${productName}`);
+    }
+}
+
+const subject = new Subject;
+
+const mailer = new Mailer('flo@test.fr');
+const wishList = new WishList;
+
+// add subscribers
+subject.subscribe(mailer);
+subject.subscribe(wishList);
+
+subject.trigger('car'); // Send email to flo@test.fr with car; add car in products
+// remove subscriber wishList
+subject.unsubscribe(wishList);
+
+subject.trigger('car'); // Send email to flo@test.fr with car;
--- creational_Design_Pattern/Proxy/implementation1.js
+++ creational_Design_Pattern/Proxy/implementation1.js
@@ -1,3 +1,4 @@
+// Fake data
 const fakeData = {
     data: [
         {"name":"Amsterdam","latLng":"52.3700° N, 4.8900° E"},
@@ -24,7 +25,7 @@ const city = 'London';
 const geoLocInst = new GeoLoc(fakeData);
 console.log(geoLocInst.getLatLng(city)); //  from API: { name: "London", latLng: "51.5171° N, 0.1062° W" }
 
-// Proxy
+// ------------- Proxy
 class ProxyGeoLoc {
     constructor() {
         this.cache = [];
@@ -32,20 +33,21 @@ class ProxyGeoLoc {
     }
 
     getLatLng(city) {
-        const cachedData = this.cache.filter((cityData) => {
+        const cachedData = this.cache.find((cityData) => {
             return cityData.name === city;
         })
-        if (cachedData[0]) {
+        if (cachedData) {
             console.log('from cache');
             return cachedData;
         }
         const data = this.geoLoc.getLatLng(city);
-        this.cache.push(data);
+        this.cache.unshift(data);
         return data;
     }
 }
 
 const proxyInst = new ProxyGeoLoc();
-proxyInst.getLatLng(city); // from API  { name: "London", latLng: "51.5171° N, 0.1062° W" }
-proxyInst.getLatLng(city); // from cache  { name: "London", latLng: "51.5171° N, 0.1062° W" }
-proxyInst.getLatLng(city); // from cache  { name: "London", latLng: "51.5171° N, 0.1062° W" }
+
+console.log(proxyInst.getLatLng(city)); // from API  { name: "London", latLng: "51.5171° N, 0.1062° W" }
+console.log(proxyInst.getLatLng(city)); // from cache  { name: "London", latLng: "51.5171° N, 0.1062° W" }
+console.log(proxyInst.getLatLng(city)); // from cache  { name: "London", latLng: "51.5171° N, 0.1062° W" }
